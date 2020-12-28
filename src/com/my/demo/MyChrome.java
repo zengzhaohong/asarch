df69af07efef59f32ca4afee113d8a117cdba52c
zengzhaohong <1115827740@qq.com>
2020-12-29 00:42:17 +0800

    version 1229 0:41

---
+++ src/com/my/demo/MyChrome.java
@@ -0,0 +1,9 @@
+package com.my.demo;
+
+public class MyChrome extends MyRemoteDriver{
+
+	public void sendkeys() {
+		System.out.println("MyChrome sendkeys");
+	}
+	
+}
---
+++ src/com/my/demo/MyRemoteDriver.java
@@ -0,0 +1,12 @@
+package com.my.demo;
+
+public class MyRemoteDriver {
+
+	public void click() {
+		System.out.println("MyRemoteDriver click");
+	}
+	
+	public void sendkeys() {
+		System.out.println("MyRemoteDriver sendkeys");
+	}
+}
